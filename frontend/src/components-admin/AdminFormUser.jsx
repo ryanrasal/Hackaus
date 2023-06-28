8e0b79d301b5d1f15605a6020a62c1b1492109c1
Sahrane_Guassemi <sahranedev@gmail.com>
2023-06-29 01:15:57 +0200

    dashbord user + form create user

---
+++ frontend/src/components-admin/AdminFormUser.jsx
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
+
+export default function AdminFormUser() {
+  const [dataUser, setDataUser] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
+  const testToaster = () => {
+    toast.success("Toast Test Successful!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+    });
+  };
+
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setDataUser((prevDataUser) => ({
+      ...prevDataUser,
+      [name]: value,
+    }));
+  };
+
+  const handleCreateUser = (e) => {
+    e.preventDefault();
+
+    if (
+      dataUser.firstname &&
+      dataUser.lastname &&
+      dataUser.email &&
+      dataUser.password
+    ) {
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      const body = JSON.stringify(dataUser);
+
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body,
+      };
+
+      fetch(`http://localhost:5000/users`, requestOptions)
+        .then((response) => {
+          console.warn(response.status);
+
+          if (response.status !== 201) {
+            throw new Error(
+              `Erreur ${response.status}: ${response.statusText}`
+            );
+          } else {
+            return response.text();
+          }
+        })
+        .then(() => {
+          toast.success("Utilisateur Créer", {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+          });
+          navigate("/admin/dashboard");
+        })
+        .catch(console.error);
+    }
+  };
+
+  const inputs = [
+    { type: "text", name: "firstname", placeholder: "Prénom" },
+    { type: "text", name: "lastname", placeholder: "Nom" },
+    { type: "email", name: "email", placeholder: "Email" },
+    { type: "password", name: "password", placeholder: "Mot de passe" },
+  ];
+
+  return (
+    <div className=" flex md:flex-col w-full flex-wrap justify-center mt-6 ">
+      <h2 className="text-2xl font-bold text-center tracking-wider mb-4">
+        Ajouter un nouvel utilisateur
+      </h2>
+      <form
+        onSubmit={handleCreateUser}
+        className="flex md:w-[70vw] md:mx-auto flex-col md:flex-row md:flex-wrap "
+      >
+        {inputs.map((input) => (
+          <input
+            key={input.name}
+            type={input.type}
+            name={input.name}
+            value={dataUser[input.name]}
+            onChange={onChange}
+            placeholder={input.placeholder}
+            className="w-80 md:w-48 my-2 md:mx-3 rounded-md placeholder:text-gray-300 border border-primary py-2 pl-4 text-lg placeholder-black"
+          />
+        ))}
+
+        <button
+          type="submit"
+          className="bg-black md:w-[20vw] md:mx-auto hover:bg-red-600 hover:text-white text-white p-3 rounded-lg"
+        >
+          Ajouter
+        </button>
+      </form>
+      <button type="button" onClick={testToaster}>
+        TOAST MOI
+      </button>
+    </div>
+  );
+}
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,6 +2,8 @@ import LoginPage from "./pages/LoginPage";
 import App from "./App";
 import Home from "./pages/Home";
 import User from "./User";
+import AdminDashboard from "./pages/AdminDashboard";
+import AdminFormUser from "./components-admin/AdminFormUser";
 
 const Routes = [
   {
@@ -21,6 +23,14 @@ const Routes = [
       {
         path: "home",
         element: <Home />,
+      },
+      {
+        path: "dashboard",
+        element: <AdminDashboard />,
+      },
+      {
+        path: "create-user",
+        element: <AdminFormUser />,
       },
     ],
   },
