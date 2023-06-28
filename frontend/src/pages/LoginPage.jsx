d06c2fd93e9caeef60b74a9cddc867a357ba797f
Sahrane_Guassemi <sahranedev@gmail.com>
2023-06-28 19:08:40 +0200

    rebuild login fuction front side

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,37 +1,60 @@
 import { useState } from "react";
 import "../App.css";
 import "react-toastify/dist/ReactToastify.css";
-import { toast } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
-  const [dataLogin, setDataLogin] = useState({
-    email: "",
-    password: "",
-  });
-
-  const handleChange = (e) => {
-    setDataLogin({ ...dataLogin, [e.target.name]: e.target.value });
-  };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (dataLogin.email && dataLogin.password) {
-      ("login", "post", dataLogin).then((res) => {
-        Cookies.set("user", JSON.stringify(res.data.user));
-        Cookies.set("token", res.data.token);
-        navigate("/");
-        toast.success(`Bienvenue ${res.data.user.firstname} !`);
-      });
-    } else {
-      console.warn("erreur !");
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const body = JSON.stringify({
+      email,
+      password,
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body,
+    };
+
+    if (email && password) {
+      fetch("http://localhost:5000/login", requestOptions)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(
+              `Erreur ${response.status}: ${response.statusText}`
+            );
+          }
+          return response.json();
+        })
+        .then((result) => {
+          Cookies.set("user", JSON.stringify(result.user));
+          Cookies.set("token", result.token);
+          toast.success(`Bienvenue ${result.user.firstname} !`);
+          navigate("home");
+        })
+        .catch((error) => {
+          toast.error(`Erreur lors de la connexion: ${error.message}`);
+        });
     }
   };
+
   return (
     <div className="h-screen bg-[#94b0c8]">
-      <div className=" flex flex-col px-4 mx-4 pt-[50%]">
+      <ToastContainer />
+      <form
+        onSubmit={handleSubmit}
+        className=" flex flex-col px-4 mx-4 pt-[50%]"
+      >
         <h1 className="text-white text-center uppercase pb-2 text-3xl tracking-wider font-semibold">
           Emmaüs Connect
         </h1>
@@ -42,26 +65,26 @@ function LoginPage() {
           <p className="font-bold pb-2 tracking-wider">Email</p>
           <input
             name="email"
-            onChange={handleChange}
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
             className="py-2 mb-3 w-full rounded-md"
             type="text"
           />
           <p className="font-bold pb-2 tracking-wider">Mot de passe</p>
           <input
             name="password"
-            onChange={handleChange}
+            onChange={(e) => setPassword(e.target.value)}
             className=" py-2 w-full rounded-md"
             type="password"
           />
         </div>
         <button
-          onClick={handleSubmit}
           className="border py-2 text-white border-white shadow-lg rounded-md"
-          type="button"
+          type="submit"
         >
           Se Connecter
         </button>
-      </div>
+      </form>
     </div>
   );
 }
