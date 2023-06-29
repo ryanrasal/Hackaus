575a9de4157bf9fd6b70d4d4db979ae8e3e7f657
IdirX69 <adil.idirr@gmail.com>
2023-06-29 10:16:14 +0200

    arrow navigate

--- frontend/src/components/NavbarTop.jsx
+++ frontend/src/components/NavbarTop.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import arrowReturn from "../assets/navbar/arrowReturn.png";
 import profil from "../assets/navbar/profil.png";
 import logo from "../assets/navbar/logo.png";
 
 export default function NavbarTop() {
+  const navigate = useNavigate();
   return (
     <div className="shadow-md bg-[#384a5a] border border-white w-full">
       <div className="flex items-center justify-between px-7">
-        <button type="button">
+        <button type="button" onClick={() => navigate(-1)}>
           <img src={arrowReturn} alt="" />
         </button>
         <img src={logo} alt="" />
