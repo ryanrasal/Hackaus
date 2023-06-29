9c5ba9c1f7c4059aa06786e401932a20d6344dae
Sahrane_Guassemi <sahranedev@gmail.com>
2023-06-29 18:03:52 +0200

    amelioration of color score with progress bar

--- frontend/src/components/Recap.jsx
+++ frontend/src/components/Recap.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable no-nested-ternary */
 import React, { useState, useEffect } from "react";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
 function Recap() {
-  const [category, setCategory] = useState("3-B");
+  const [category, setCategory] = useState("1-B");
   const [score, setScore] = useState(4);
 
   const [maxScore, setMaxScore] = useState(5);
@@ -16,9 +17,16 @@ function Recap() {
 
   const percentage = (score / maxScore) * 100;
 
-  const colorGradient =
-    // eslint-disable-next-line no-nested-ternary
-    percentage > 60 ? "green" : percentage > 30 ? "[#eab308]" : "red";
+  let colorGradient;
+  if (percentage > 90) {
+    colorGradient = "green";
+  } else if (percentage > 60) {
+    colorGradient = "#6be338";
+  } else if (percentage > 30) {
+    colorGradient = "#eab308";
+  } else {
+    colorGradient = "red";
+  }
 
   useEffect(() => {
     const newScore = parseInt(category.split("-")[0], 10);
@@ -26,14 +34,33 @@ function Recap() {
   }, [category]);
 
   return (
-    <div>
+    <div className="h-[850px]">
+      <div>
+        <p>Système d'exploitation : </p>
+        <p>RAM : </p>
+        <p>Capacité de Stockage </p>
+      </div>
+      <div className="flex justify-around">
+        <button
+          type="button"
+          className="w-auto border-2 border-black px-4 text-amber-300 bg-blue-950 rounded-lg"
+        >
+          Très Bon Etat
+        </button>
+        <button
+          type="button"
+          className="w-auto border-2 border-black px-4 text-amber-300 bg-blue-950 rounded-lg"
+        >
+          Câble de chargement
+        </button>
+      </div>
       <div className="flex justify-center w-full mb-12">
-        <div className="w-64 h-64 bg-[#002743] flex flex-col mt-[122px]  rounded-full  text-center p-10  ">
+        <div className="w-64 h-64 bg-[#002743] flex flex-col mt-[125px]  rounded-full  text-center p-10  ">
           <div className="text-white text-2xl">Catégorie</div>
           <div className="text-white text-6xl font-bold mt-10">{category}</div>
         </div>
 
-        <div className="absolute top-44">
+        <div className="absolute top-[280px]">
           <CircularProgressbar
             value={percentage}
             styles={buildStyles({
@@ -50,13 +77,13 @@ function Recap() {
         </div>
       </div>
       <div className="flex justify-around">
-        <div className="rounded-full h-12 w-12 bg-[#00ACB0] flex items-center justify-center">
+        <div className="rounded-full h-12 w-12 text-white bg-[#00ACB0] flex items-center justify-center">
           45 {/* {antutuScore} */}
         </div>
-        <div className="rounded-full h-12 w-12 bg-[#00ACB0] flex items-center justify-center">
+        <div className="rounded-full h-12 w-12 text-white bg-[#00ACB0] flex items-center justify-center">
           90 {/* {ramScore} */}
         </div>
-        <div className="rounded-full h-12 w-12 bg-[#00ACB0] flex items-center justify-center">
+        <div className="rounded-full h-12 w-12 text-white bg-[#00ACB0] flex items-center justify-center">
           30 {/* {stockageScore} */}
         </div>
       </div>
